1bcb2049eb40c44bed1cec6a4e6f0514dec3bf27
takuya furuyama <muscat20020915@gmail.com>
2026-07-01 11:46:39 +0900

    init

---
+++ backend/src/main/java/com/example/librashare/LibrashareApplication.java
@@ -0,0 +1,13 @@
+package com.example.librashare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrashareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrashareApplication.class, args);
+	}
+
+}
