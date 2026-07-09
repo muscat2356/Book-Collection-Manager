9991884ccd2cfb094c1297e162abe1e07cb67a95
takuya furuyama <muscat20020915@gmail.com>
2026-07-09 17:29:07 +0900

    yml/docker-compose/converterの修正

--- backend/src/main/java/com/example/librashare/security/JwtAuthConverter.java
+++ backend/src/main/java/com/example/librashare/security/JwtAuthConverter.java
@@ -27,19 +27,19 @@ public class JwtAuthConverter implements Converter<Jwt, AbstractAuthenticationTo
 
     @Override
     public AbstractAuthenticationToken convert(@NonNull Jwt jwt){
-
+        System.out.println("Convertメソッドが呼ばれました");
         Collection<GrantedAuthority> authorities = Stream.concat(
             jwtGrantedAuthoritiesConverter.convert(jwt).stream(), 
             extractRealmsRole(jwt).stream()
             ).collect(Collectors.toSet());
-
+        System.out.println("Convertメソッドが終了しました");
         return new JwtAuthenticationToken(jwt, authorities);
     }
 
     private Collection<GrantedAuthority> extractRealmsRole(Jwt jwt) {
         Map<String, Object> realmAccess;
         Collection<String> realmRoles;
-
+        System.out.println("extractメソッドが開始しました");
         Object rawRealmAccess = jwt.getClaim("realm_access");
         if (!(rawRealmAccess instanceof Map)) {
             return Set.of();
@@ -47,13 +47,13 @@ public class JwtAuthConverter implements Converter<Jwt, AbstractAuthenticationTo
 
         realmAccess = (Map<String, Object>)rawRealmAccess;
 
-        Object rawRealmRoles = jwt.getClaim("realm_roles");
+        Object rawRealmRoles = realmAccess.get("roles");
         if (!(rawRealmRoles instanceof Collection)) {
             return Set.of();
         }
 
         realmRoles = (Collection<String>) rawRealmRoles;
-
+        System.out.println("extractメソッドが終了しました");
         return realmRoles.stream()
                         .map(role -> new SimpleGrantedAuthority("ROLE_"+role))
                         .collect(Collectors.toSet());
