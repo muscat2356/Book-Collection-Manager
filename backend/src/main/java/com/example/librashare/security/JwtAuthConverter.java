50e7e2a094bdc3a58b32c3a9a6216767486730e5
takuya furuyama <muscat20020915@gmail.com>
2026-07-09 16:09:47 +0900

    401エラーの対応/環境変数の修正

--- backend/src/main/java/com/example/librashare/security/JwtAuthConverter.java
+++ backend/src/main/java/com/example/librashare/security/JwtAuthConverter.java
@@ -14,10 +14,12 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.stereotype.Component;
 
 /**
  * JwtAuthConverter
  */
+@Component
 public class JwtAuthConverter implements Converter<Jwt, AbstractAuthenticationToken> {
 
     private final JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter =
