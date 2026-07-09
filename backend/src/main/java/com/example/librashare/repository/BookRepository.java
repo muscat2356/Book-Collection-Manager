6a8cc58e343c722ec24e2b68a527a76bfb95432f
takuya furuyama <muscat20020915@gmail.com>
2026-07-09 11:29:38 +0900

    ymlファイルの修正 dockerコマンドメモの追加

--- backend/src/main/java/com/example/librashare/repository/BookRepository.java
+++ backend/src/main/java/com/example/librashare/repository/BookRepository.java
@@ -1,9 +1,11 @@
 package com.example.librashare.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.librashare.domain.Book;
 
+@Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
 
 }
