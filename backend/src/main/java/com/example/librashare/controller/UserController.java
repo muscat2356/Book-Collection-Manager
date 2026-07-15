311113e153752bb1f6e5482bf55b1e0c4bf8b723
muscat0915 <muscat20020915@gmail.com>
2026-07-15 17:14:48 +0900

    usercontroller実装

--- backend/src/main/java/com/example/librashare/controller/UserController.java
+++ backend/src/main/java/com/example/librashare/controller/UserController.java
@@ -1,17 +1,32 @@
 package com.example.librashare.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.librashare.domain.User;
+import com.example.librashare.dto.request.UserRequest;
 import com.example.librashare.dto.response.UserResponse;
 import com.example.librashare.service.UserService;
 
+import jakarta.validation.Valid;
+
+/**
+ * UserAPIの処理
+ * @author furuyama
+ * @since 2026-07-14
+ * @see UserService
+ */
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
@@ -22,6 +37,10 @@ public class UserController {
         this.userService = userService;
     }
 
+    /**
+     * ユーザー全件取得API
+     * @return　ユーザー全件のjsonデータをレスポンス
+     */
     @GetMapping
     @PreAuthorize("hasAnyRole('general_employee','admin_employee')")
     public ResponseEntity<List<UserResponse>> findAll(){
@@ -41,4 +60,91 @@ public class UserController {
         return ResponseEntity.ok(responses);
     }
 
+    /**
+     * 該当ユーザー取得API
+     * @param id
+     * @return　該当ユーザーのjsonデータ/存在しない場合に404/notfoundをレスポンス
+     */
+    @GetMapping("/{id}")
+    @PreAuthorize("hasAnyRole('general_employee','admin_employee')")
+    public ResponseEntity<UserResponse> findById(@PathVariable Long id){
+
+        Optional<User> find = userService.findById(id);
+
+        if(find.isEmpty()){
+            return ResponseEntity.notFound().build();
+        }
+
+        User user = new User();
+
+        user = find.get();
+
+        UserResponse response = new UserResponse();
+        response.setId(user.getId());
+        response.setKeycloakSub(user.getKeycloakSub());
+        response.setDisplayName(user.getDisplayName());
+        response.setEmail(user.getEmail());
+        response.setActive(user.isActive());
+
+        return ResponseEntity.ok(response);
+
+    }
+
+    /**
+     * 該当ユーザー登録API
+     * @param UserRequest userRequest
+     * @return　Response 201　作成したユーザーの情報を送信
+     */
+    @PostMapping
+    @PreAuthorize("hasAnyRole('general_employee','admin_employee')")
+    public ResponseEntity<UserResponse> createUser(@Valid @RequestBody UserRequest userRequest){
+        
+        User user = userService.create(userRequest.getDisplayName(), userRequest.getEmail());
+        
+        UserResponse response = new UserResponse();
+        response.setId(user.getId());
+        response.setKeycloakSub(user.getKeycloakSub());
+        response.setDisplayName(user.getDisplayName());
+        response.setEmail(user.getEmail());
+        response.setActive(user.isActive());
+
+        return ResponseEntity.ok(response);
+
+    }
+    
+    /**
+     * 該当ユーザーの全更新処理API
+     * @param id
+     * @param userRequest
+     * @return　成功時：200 存在しない場合：404
+     */
+    @PutMapping("/{id}")
+    @PreAuthorize("hasAnyRole('general_employee','admin_employee')")
+    public ResponseEntity<UserResponse> update(@PathVariable Long id, @Valid @RequestBody UserRequest userRequest){
+
+        User user = userService.update(id, userRequest.getDisplayName(), userRequest.getEmail());
+
+        UserResponse response = new UserResponse();
+        response.setId(user.getId());
+        response.setKeycloakSub(user.getKeycloakSub());
+        response.setDisplayName(user.getDisplayName());
+        response.setEmail(user.getEmail());
+        response.setActive(user.isActive());
+
+        return ResponseEntity.ok(response);
+    }
+
+    /**
+     * 該当ユーザーの削除処理API
+     * @param id
+     * @return
+     */
+    @DeleteMapping("/{id}")
+    @PreAuthorize("hasAnyRole('general_employee','admin_employee')")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id){
+
+        userService.deleteUser(id);
+        
+        return ResponseEntity.noContent().build();
+    }
 }
--- backend/src/main/java/com/example/librashare/service/UserService.java
+++ backend/src/main/java/com/example/librashare/service/UserService.java
@@ -12,6 +12,13 @@ import com.example.librashare.repository.UserRepository;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 
+/**
+ * ユーザーのCRUD機能を実装したService
+ * @author furuyama
+ * @since 2026-07-15
+ * @see 
+ * UserRepository
+ */
 @Service
 public class UserService {
 
@@ -23,17 +30,28 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
-    //findAll
+    /**
+     * ユーザーの全件検索
+     * @return　DBからユーザーを全件リターン
+     */
     public List<User> findAll(){
         return userRepository.findAll();
     }
 
-    //findById
+    /**
+     * 該当ユーザーの検索
+     * @param id
+     * @return　DBから該当書籍IDをリターン（レスポンスで使用するため）
+     */
     public Optional<User> findById(Long id){
         return userRepository.findById(id);
     }
 
-    //create
+    /**
+     * 該当ユーザー登録Service
+     * @param email,displayName
+     * @return　登録ユーザーのリターン
+     */
     @Transactional
     public User create(String email,String displayName){
 
@@ -44,7 +62,12 @@ public class UserService {
 
     }
 
-    //update
+    /**
+     * 該当ユーザーの更新処理service側
+     * @param id
+     * @param id, displayName,　email
+     * @return　ユーザー情報の送信
+     */
     @Transactional
     public User update(Long id, String displayName, String email){
 
@@ -62,7 +85,10 @@ public class UserService {
         return user;
     }
 
-    //delete
+    /**
+     * 該当ユーザーの論理削除（EnableOFF設定）
+     * @param id
+     */
     @Transactional
     public void deleteUser(Long id){
         Optional<User> optinalUser = userRepository.findById(id);
